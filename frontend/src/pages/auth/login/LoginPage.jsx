e730e38a598da4c7d308dcc56881682bbf3cad19
Francelle Sarayan <lorrainesarayan@gmail.com>
2026-01-15 23:46:51 +0800

    login page kinda  okay

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -1,5 +1,8 @@
-import { use } from "react"
 import { useState } from "react"
+import { Link } from "react-router-dom";
+import { IoPersonOutline  } from "react-icons/io5";
+import { CiLock } from "react-icons/ci";
+
 
 
 const LoginPage = () => {
@@ -22,24 +25,76 @@ const LoginPage = () => {
 
   return ( // here irrender yung html
     <div className="min-h-screen flex items-center justify-center bg-base-100 p-6">
+
+
+      <div className="absolute top-2 left-6 flex items-center gap-3">
+        <div className="w-15 h-15 rounded-full overflow-hidden border-none">
+            <img 
+                src="Logos/logo-1.png" 
+                alt="Logo Icon" 
+                className="w-full h-full object-cover border-none" />
+        </div>
+        <div className="w-40 h-20  overflow-hidden border-none">
+          <img 
+            src="Logos/logo-2.png" 
+            alt="Logo Icon" 
+            className="w-full h-full object-cover border-none" />
+        </div>
+      </div>
+
+      
+      
+      <form onSubmit={handleSubmit} className="w-full max-w-md">
+
+        <fieldset className="fieldset text-black fieldset-lg bg-[#A4C0ED] rounded-box w-full max-w-md py-10 px-4 flex flex-col items-center text-black">
+        <div className="fieldset-legend flex flex-col items-center w-full mb-2">
+          <h2 className="text-xl font-bold">Welcome Back</h2>
+          <p className="text-sm font-normal">Login to Continue</p>
+        </div>
         
-        <fieldset className="fieldset fieldset-lg bg-[#A4C0ED] rounded-box w-full max-w-md p-4 flex flex-col items-center">
-        <legend className="fieldset-legend flex flex-col items-center w-full">
-          <span className="text-xl font-normal">Welcome Back</span>
-          <span className="text-sm font-bold">Login to Continue</span>
-        </legend>
+  
+        <label className="input bg-white rounded-[40px] flex items-center gap-2 w-full max-w-xs border-none">
+          <IoPersonOutline className="text-gray-500" />
+          <input 
+            type="username" 
+            name="username"
+            className="grow text-black" 
+            placeholder="Enter Username" 
+            onChange={handleInputChange} 
+            value={formData.username}
+          />
+        </label>
 
-          <input type="email" className="input bg-white rounded-[40px]" placeholder="Email" />
-          <input type="password" className="input bg-white rounded-[40px]" placeholder="Password" />
+        <label className="input bg-white rounded-[40px] flex items-center gap-2 w-full max-w-xs border-none">
+          <CiLock className="text-gray-500" />
+          <input 
+            type="password" 
+            name="password"
+            className="grow text-black" 
+            placeholder="Enter Password" 
+            onChange={handleInputChange}
+            value={formData.password}
+          />
+        </label>
 
-          <button className="btn btn-neutral mt-4 rounded-[40px] w-30">Login</button>
-          <a href="/ForgotPassword" className="link link-hover text-sm "> Forgot Password</a>
+
+        {/* no linked route with this button, i need to ensure security sa with backend pa aayusin*/}
+        <button type="submit" className="btn btn-neutral bg-[#F6FFDE] mt-4 border-none rounded-[40px] w-30 mb-2">Login</button>
+        <Link 
+          to="/ForgotPassword" 
+          className="link link-hover text-sm text-black"> Forgot Password </Link>
         </fieldset>
 
+        <div className="flex flex-col items-center gap-1 mt-4">
+          <p className="text-sm text-black"> {`New User? `}
+            <Link 
+              to='/signup' 
+              className="text-black hover:underline hover:font-bold transition-all"> Sign up
+            </Link>
+          </p>
+        </div>
 
-
-
-
+      </form> 
     </div>
   )
 }
